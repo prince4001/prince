534f0b60f9e6240a065c27ff6213f9d225e31375
prince4001 <65185089+prince4001@users.noreply.github.com>
2020-05-13 12:26:04 +0500

    Update zeroboy.py

--- .github/workflows/zeroboy.py
+++ .github/workflows/zeroboy.py
@@ -49,17 +49,18 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•prince - tv -OFFICAL------•◈•
+|__\~~) •||•kashif - ali---------------•◈•
+|__(-----\  •◈•------prince-Tiger --------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;....
+
+\033[1;96mInstagram \033[1;93m: \033[1;....
+\033[1;96mFacebook  \033[1;93m: \033[1; mewish.noor.961
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/prince4001
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +81,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92m....
+\033[1;96mInstagram \033[1;93m: \033[1;92m....
+\033[1;96mFacebook  \033[1;93m: \033[1;92m mewish.noor.961
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/prince4001
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "kashif"
+CorrectPassword = "ali"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +101,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://youtu.be/lHVwldU7LfE')
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://youtu.be/lHVwldU7LfE')
 
 def login():
 	os.system('clear')
